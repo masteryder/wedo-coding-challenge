7c4d83cd4436a04636d6bbbb45c8b98e4ccbeb3e
Joao Ventura <joao.ventura@buzzbrothers.ch>
2021-05-22 13:49:01 +0200

    Pass basic tests for simple cost calculation

--- src/__libs/calc.ts
+++ src/__libs/calc.ts
@@ -1,9 +1,23 @@
+import { frequencyConfig } from "../__config/frequency.config";
+
 export function simpleCalculateCost(
   nbParticipants: number,
   averageSalary: number,
   meetingDuration: number,
   meetingFrequency: number): number {
+  if(nbParticipants <= 0 || averageSalary <=0 || meetingDuration <= 0 || meetingFrequency <= 0){
+    return 0;
+  }
 
-  return -1;
+  let totalSalaries: number = nbParticipants * averageSalary;
 
+  let meetingTimeHours: number = meetingDuration * meetingFrequency * nbParticipants;
+
+  let totalHours: number = nbParticipants * frequencyConfig.dailyHours * frequencyConfig.daily;
+
+  let percentage: number = meetingTimeHours / totalHours;
+
+  let cost = totalSalaries * percentage;
+
+  return cost;
 }
