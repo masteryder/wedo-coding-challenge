aea2e5d1fd2fa99bc76816851bc082730e67f055
Joao Ventura <joao.ventura@buzzbrothers.ch>
2021-05-22 15:07:14 +0200

    Add some cool basic transitions

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
 	import Input from "./components/1-atoms/Input.svelte";
 	import type { iFrequency } from "./__models/iFrequency";
 	import { calculateSavings, simpleCalculateCost } from "./__libs/calc";
+	import { fade, fly } from 'svelte/transition';
 
 	export let nbParticipants: number = 1;
 	export let averageSalary: number = 50000;
@@ -59,61 +60,65 @@
 	</div>
 
 	{#if mode == Modes.SIMPLE}
-		<Input
-			name="average-salary"
-			label="Average annual salary of participants"
-			type="text"
-			icon='<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+		<div out:fade="{{ duration: 300}}" in:fly={{x:200, duration: 300, delay: 300}}>
+			<Input
+				name="average-salary"
+				label="Average annual salary of participants"
+				type="text"
+				icon='<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+							</svg>'
+				id="data-average-salary"
+				placeholder="0.00"
+				unit="CHF"
+				bind:value={averageSalary}
+			/>
+
+			<Input
+				name="number-participants"
+				label="Number of participants"
+				type="text"
+				icon='<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
 						</svg>'
-			id="data-average-salary"
-			placeholder="0.00"
-			unit="CHF"
-			bind:value={averageSalary}
-		/>
+				id="data-number-participants"
+				bind:value={nbParticipants}
+				placeholder="0"
+			/>
 
-		<Input
-			name="number-participants"
-			label="Number of participants"
-			type="text"
-			icon='<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-					</svg>'
-			id="data-number-participants"
-			bind:value={nbParticipants}
-			placeholder="0"
-		/>
+			<Input
+				name="meeting-duration"
+				label="Duration of meeting"
+				type="text"
+				icon='<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+						</svg>'
+				id="data-meeting-duration"
+				unit="Hour(s)"
+				bind:value={meetingDuration}
+				placeholder="0"
+			/>
 
-		<Input
-			name="meeting-duration"
-			label="Duration of meeting"
-			type="text"
-			icon='<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-					</svg>'
-			id="data-meeting-duration"
-			unit="Hour(s)"
-			bind:value={meetingDuration}
-			placeholder="0"
-		/>
-
-		<div class="text-sm">Frequency</div>
-		<div class="mb-2">
-			{#each availableFrequencies as f}
-				<label class="block">
-					<input type="radio" bind:group={frequency} value={f.repetitions}>
-					{f.label}
-				</label>
-			{/each}
-		</div>
-		{#if !isNaN(total) && Math.floor(savings) > 0}
-			<div>
-				<p class="text-center">You are currently spending <b class="text-red-600">{total.toFixed(2)} CHF</b> on meetings in a year 😅</p>
-				<p class="text-center text-2xl">You could be saving <b class="text-green-600">{savings.toFixed(2)} CHF</b> just by using Wedo 👌</p>
+			<div class="text-sm">Frequency</div>
+			<div class="mb-2">
+				{#each availableFrequencies as f}
+					<label class="block">
+						<input type="radio" bind:group={frequency} value={f.repetitions}>
+						{f.label}
+					</label>
+				{/each}
 			</div>
-		{/if}
+			{#if !isNaN(total) && Math.floor(savings) > 0}
+				<div>
+					<p class="text-center">😅 You are currently spending <b class="text-red-600">{total.toFixed(2)} CHF</b> on meetings in a year</p>
+					<p class="text-center text-2xl">👌 You could be saving <b class="text-green-600">{savings.toFixed(2)} CHF</b> just by using <a href="https://wedo.swiss"><img class="inline w-20" src="assets/images/logo-wedo.png" alt="Wedo"/></a></p>
+				</div>
+			{/if}
+		</div>
 	{:else if mode == Modes.PRO}
-			PRO
+			<div out:fade="{{ duration: 300}}" in:fly={{x:200, duration: 300, delay: 300}}>
+				PRO
+			</div>
 	{/if}
 
 </main>
