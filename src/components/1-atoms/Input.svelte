edc893076ecaf6efdf73f989888ff896c0f45b04
Joao Ventura <joao.ventura@buzzbrothers.ch>
2021-05-22 14:07:02 +0200

    Fix issue with input binding (because of dynamic type)

--- src/components/1-atoms/Input.svelte
+++ src/components/1-atoms/Input.svelte
@@ -7,6 +7,12 @@
   export let placeholder: string;
   export let unit: string = null;
   export let value: any;
+
+  const handleInput = e => {
+    value = type.match(/^(number|range)$/)
+      ? +e.target.value
+      : e.target.value;
+  };
 </script>
 
 <div>
@@ -19,7 +25,7 @@
       </span>
     </div>
     {/if}
-    <input type="{type}" name="{name}" id="{id}" class="focus:ring-indigo-500 block w-full{icon != null ? ' pl-10' : ''} pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="{placeholder}" value="{value}">
+    <input {type} {name} {id} class="focus:ring-indigo-500 block w-full{icon != null ? ' pl-10' : ''} pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="{placeholder}" {value} on:input={handleInput}>
     {#if unit != null}
       <div class="absolute inset-y-0 right-0 flex items-center">
         <div class="my-auto py-0 pl2 pr-4 text-gray-500 sm:text-sm rounded-md">{unit}</div>
